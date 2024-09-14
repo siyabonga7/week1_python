0062b8d4dbb6c16defeb504ef60f6828237ae9a0
siyabongahlatshwayo <siyabonga10759@gmail.com>
2024-09-15 01:41:40 +0200

    Add assignment to program

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# The details
+name = "Siyabonga"
+age = "25"
+location = "Johannesburg"
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
